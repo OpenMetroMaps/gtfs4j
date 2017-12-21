af49a616b2f02a1dfd20459371c0895b97252843
Sebastian Kürten <sebastian@topobyte.de>
2017-12-21 18:30:31 +0100

    Add filenames of feed files

---
+++ csv-reader/src/main/java/org/openmetromaps/gtfs4j/GtfsFiles.java
@@ -0,0 +1,40 @@
+// Copyright 2017 Sebastian Kuerten
+//
+// This file is part of gtfs4j.
+//
+// gtfs4j is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// gtfs4j is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with gtfs4j. If not, see <http://www.gnu.org/licenses/>.
+
+package org.openmetromaps.gtfs4j;
+
+public class GtfsFiles
+{
+
+	// Required files
+	public static String NAME_AGENCY = "agency.txt";
+	public static String NAME_STOPS = "stops.txt";
+	public static String NAME_ROUTES = "routes.txt";
+	public static String NAME_TRIPS = "trips.txt";
+	public static String NAME_STOP_TIMES = "stop_times.txt";
+	public static String NAME_CALENDAR = "calendar.txt";
+
+	// Optional files
+	public static String NAME_CALENDAR_DATES = "calendar_dates.txt";
+	public static String NAME_FARE_ATTRIBUTES = "fare_attributes.txt";
+	public static String NAME_FARE_RULES = "fare_rules.txt";
+	public static String NAME_SHAPES = "shapes.txt";
+	public static String NAME_FREQUENCIES = "frequencies.txt";
+	public static String NAME_TRANSFERS = "transfers.txt";
+	public static String NAME_FEED_INFO = "feed_info.txt";
+
+}
