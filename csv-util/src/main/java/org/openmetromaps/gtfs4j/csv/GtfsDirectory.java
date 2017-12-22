e2f64fe71f3fa2d13a095557c740747d72f84327
Sebastian Kürten <sebastian@topobyte.de>
2017-12-22 12:32:16 +0100

    Add new module csv-util
    Extract GtfsDirectory class from csvreader/Test that encapsulates
    utility methods for reading all objects from a specific type from a
    directory with GTFS data files.

---
+++ csv-util/src/main/java/org/openmetromaps/gtfs4j/csv/GtfsDirectory.java
@@ -0,0 +1,127 @@
+// Copyright 2017 Sebastian Kuerten
+//
+// This file is part of gtfs4j.
+//
+// gtfs4j is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// gtfs4j is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with gtfs4j. If not, see <http://www.gnu.org/licenses/>.
+
+package org.openmetromaps.gtfs4j.csv;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+import org.openmetromaps.gtfs4j.csv.GtfsFiles;
+import org.openmetromaps.gtfs4j.csvreader.AgencyReader;
+import org.openmetromaps.gtfs4j.csvreader.CalendarReader;
+import org.openmetromaps.gtfs4j.csvreader.RoutesReader;
+import org.openmetromaps.gtfs4j.csvreader.StopTimesReader;
+import org.openmetromaps.gtfs4j.csvreader.StopsReader;
+import org.openmetromaps.gtfs4j.csvreader.TripsReader;
+import org.openmetromaps.gtfs4j.model.Agency;
+import org.openmetromaps.gtfs4j.model.Calendar;
+import org.openmetromaps.gtfs4j.model.Route;
+import org.openmetromaps.gtfs4j.model.Stop;
+import org.openmetromaps.gtfs4j.model.StopTime;
+import org.openmetromaps.gtfs4j.model.Trip;
+
+public class GtfsDirectory
+{
+
+	private Path path;
+
+	public GtfsDirectory(Path path)
+	{
+		this.path = path;
+	}
+
+	private BufferedReader reader(Path path) throws IOException
+	{
+		return Files.newBufferedReader(path, StandardCharsets.UTF_8);
+	}
+
+	private Path path(GtfsFiles gtfsFile)
+	{
+		return path.resolve(gtfsFile.getFilename());
+	}
+
+	public List<Agency> readAgency() throws IOException
+	{
+		Path path = path(GtfsFiles.AGENCY);
+		BufferedReader br = reader(path);
+
+		AgencyReader reader = new AgencyReader(br);
+		List<Agency> list = reader.readAll();
+		br.close();
+		return list;
+	}
+
+	public List<Stop> readStops() throws IOException
+	{
+		Path path = path(GtfsFiles.STOPS);
+		BufferedReader br = reader(path);
+
+		StopsReader reader = new StopsReader(br);
+		List<Stop> list = reader.readAll();
+		br.close();
+		return list;
+	}
+
+	public List<Route> readRoutes() throws IOException
+	{
+		Path path = path(GtfsFiles.ROUTES);
+		BufferedReader br = reader(path);
+
+		RoutesReader reader = new RoutesReader(br);
+		List<Route> list = reader.readAll();
+		br.close();
+		return list;
+	}
+
+	public List<Trip> readTrips() throws IOException
+	{
+		Path path = path(GtfsFiles.TRIPS);
+		BufferedReader br = reader(path);
+
+		TripsReader reader = new TripsReader(br);
+		List<Trip> list = reader.readAll();
+		br.close();
+		return list;
+	}
+
+	public List<StopTime> readStopTimes() throws IOException
+	{
+		Path path = path(GtfsFiles.STOP_TIMES);
+		BufferedReader br = reader(path);
+
+		StopTimesReader reader = new StopTimesReader(br);
+		List<StopTime> list = reader.readAll();
+		br.close();
+		return list;
+	}
+
+	public List<Calendar> readCalendar() throws IOException
+	{
+		Path path = path(GtfsFiles.CALENDAR);
+		BufferedReader br = reader(path);
+
+		CalendarReader reader = new CalendarReader(br);
+		List<Calendar> list = reader.readAll();
+		br.close();
+		return list;
+	}
+
+}
--- testing/src/main/java/org/openmetromaps/gtfs4j/csvreader/Test.java
+++ testing/src/main/java/org/openmetromaps/gtfs4j/csvreader/Test.java
@@ -17,15 +17,11 @@
 
 package org.openmetromaps.gtfs4j.csvreader;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
-import org.openmetromaps.gtfs4j.csv.GtfsFiles;
+import org.openmetromaps.gtfs4j.csv.GtfsDirectory;
 import org.openmetromaps.gtfs4j.model.Agency;
 import org.openmetromaps.gtfs4j.model.Calendar;
 import org.openmetromaps.gtfs4j.model.Route;
@@ -36,82 +32,36 @@ import org.openmetromaps.gtfs4j.model.Trip;
 public class Test
 {
 
-	public static Path path = Paths.get("/tmp/gtfs");
-
-	private static BufferedReader reader(Path path) throws IOException
-	{
-		return Files.newBufferedReader(path, StandardCharsets.UTF_8);
-	}
-
-	private static Path path(GtfsFiles gtfsFile)
-	{
-		return Test.path.resolve(gtfsFile.getFilename());
-	}
+	public static GtfsDirectory dir = new GtfsDirectory(Paths.get("/tmp/gtfs"));
 
 	public static List<Agency> readAgency() throws IOException
 	{
-		Path path = path(GtfsFiles.AGENCY);
-		BufferedReader br = reader(path);
-
-		AgencyReader reader = new AgencyReader(br);
-		List<Agency> list = reader.readAll();
-		br.close();
-		return list;
-	}
-
-	public static List<Route> readRoutes() throws IOException
-	{
-		Path path = path(GtfsFiles.ROUTES);
-		BufferedReader br = reader(path);
-
-		RoutesReader reader = new RoutesReader(br);
-		List<Route> list = reader.readAll();
-		br.close();
-		return list;
-	}
-
-	public static List<Trip> readTrips() throws IOException
-	{
-		Path path = path(GtfsFiles.TRIPS);
-		BufferedReader br = reader(path);
-
-		TripsReader reader = new TripsReader(br);
-		List<Trip> list = reader.readAll();
-		br.close();
-		return list;
+		return dir.readAgency();
 	}
 
 	public static List<Stop> readStops() throws IOException
 	{
-		Path path = path(GtfsFiles.STOPS);
-		BufferedReader br = reader(path);
+		return dir.readStops();
+	}
 
-		StopsReader reader = new StopsReader(br);
-		List<Stop> list = reader.readAll();
-		br.close();
-		return list;
+	public static List<Route> readRoutes() throws IOException
+	{
+		return dir.readRoutes();
+	}
+
+	public static List<Trip> readTrips() throws IOException
+	{
+		return dir.readTrips();
 	}
 
 	public static List<StopTime> readStopTimes() throws IOException
 	{
-		Path path = path(GtfsFiles.STOP_TIMES);
-		BufferedReader br = reader(path);
-
-		StopTimesReader reader = new StopTimesReader(br);
-		List<StopTime> list = reader.readAll();
-		br.close();
-		return list;
+		return dir.readStopTimes();
 	}
 
 	public static List<Calendar> readCalendar() throws IOException
 	{
-		Path path = path(GtfsFiles.CALENDAR);
-		BufferedReader br = reader(path);
-
-		CalendarReader reader = new CalendarReader(br);
-		List<Calendar> list = reader.readAll();
-		br.close();
-		return list;
+		return dir.readCalendar();
 	}
 
 }
