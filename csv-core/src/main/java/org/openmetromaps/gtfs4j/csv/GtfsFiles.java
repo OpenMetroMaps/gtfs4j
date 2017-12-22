dfd6efa80cd37ba71020b8815a5ca31280e66659
Sebastian Kürten <sebastian@topobyte.de>
2017-12-22 11:27:59 +0100

    GtfsFiles: link to field class

--- csv-core/src/main/java/org/openmetromaps/gtfs4j/csv/GtfsFiles.java
+++ csv-core/src/main/java/org/openmetromaps/gtfs4j/csv/GtfsFiles.java
@@ -20,29 +20,32 @@ package org.openmetromaps.gtfs4j.csv;
 public enum GtfsFiles {
 
 	// Required files
-	AGENCY("agency.txt", true),
-	STOPS("stops.txt", true),
-	ROUTES("routes.txt", true),
-	TRIPS("trips.txt", true),
-	STOP_TIMES("stop_times.txt", true),
-	CALENDAR("calendar.txt", true),
+	AGENCY("agency.txt", true, Agencies.class),
+	STOPS("stops.txt", true, Stops.class),
+	ROUTES("routes.txt", true, Routes.class),
+	TRIPS("trips.txt", true, Trips.class),
+	STOP_TIMES("stop_times.txt", true, StopTimes.class),
+	CALENDAR("calendar.txt", true, Calendars.class),
 
 	// Optional files
-	CALENDAR_DATES("calendar_dates.txt", false),
-	FARE_ATTRIBUTES("fare_attributes.txt", false),
-	FARE_RULES("fare_rules.txt", false),
-	SHAPES("shapes.txt", false),
-	FREQUENCIES("frequencies.txt", false),
-	TRANSFERS("transfers.txt", false),
-	FEED_INFO("feed_info.txt", false);
+	CALENDAR_DATES("calendar_dates.txt", false, null),
+	FARE_ATTRIBUTES("fare_attributes.txt", false, null),
+	FARE_RULES("fare_rules.txt", false, null),
+	SHAPES("shapes.txt", false, null),
+	FREQUENCIES("frequencies.txt", false, null),
+	TRANSFERS("transfers.txt", false, null),
+	FEED_INFO("feed_info.txt", false, null);
 
 	private String filename;
 	private boolean required;
+	private Class<? extends Field> clazz;
 
-	private GtfsFiles(String filename, boolean required)
+	private GtfsFiles(String filename, boolean required,
+			Class<? extends Field> clazz)
 	{
 		this.filename = filename;
 		this.required = required;
+		this.clazz = clazz;
 	}
 
 	public String getFilename()
@@ -53,6 +56,11 @@ public enum GtfsFiles {
 	public boolean isRequired()
 	{
 		return required;
+	}
+
+	public Class<? extends Field> getFieldClass()
+	{
+		return clazz;
 	}
 
 }
