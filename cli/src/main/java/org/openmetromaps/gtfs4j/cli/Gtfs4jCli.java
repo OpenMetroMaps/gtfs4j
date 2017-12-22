53b17e7fa5f2a7a089d1b0aab55ed212eb69f9e4
Sebastian Kürten <sebastian@topobyte.de>
2017-12-22 10:17:53 +0100

    Add a first task 'show-info' to the CLI

---
+++ cli/src/main/java/org/openmetromaps/gtfs4j/cli/Gtfs4jCli.java
@@ -0,0 +1,64 @@
+// Copyright 2017 Sebastian Kuerten
+//
+// This file is part of OpenMetroMaps.
+//
+// OpenMetroMaps is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// OpenMetroMaps is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with OpenMetroMaps. If not, see <http://www.gnu.org/licenses/>.
+
+package org.openmetromaps.gtfs4j.cli;
+
+import de.topobyte.utilities.apache.commons.cli.commands.ArgumentParser;
+import de.topobyte.utilities.apache.commons.cli.commands.ExeRunner;
+import de.topobyte.utilities.apache.commons.cli.commands.ExecutionData;
+import de.topobyte.utilities.apache.commons.cli.commands.RunnerException;
+import de.topobyte.utilities.apache.commons.cli.commands.options.DelegateExeOptions;
+import de.topobyte.utilities.apache.commons.cli.commands.options.ExeOptions;
+import de.topobyte.utilities.apache.commons.cli.commands.options.ExeOptionsFactory;
+
+public class Gtfs4jCli
+{
+
+	public static ExeOptionsFactory OPTIONS_FACTORY = new ExeOptionsFactory() {
+
+		@Override
+		public ExeOptions createOptions()
+		{
+			DelegateExeOptions options = new DelegateExeOptions();
+			options.addCommand("show-info", RunShowInfo.OPTIONS_FACTORY,
+					RunShowInfo.class);
+			return options;
+		}
+
+	};
+
+	public static void main(String[] args) throws RunnerException
+	{
+		String name = "gtfs4j";
+
+		ExeOptions options = OPTIONS_FACTORY.createOptions();
+		ArgumentParser parser = new ArgumentParser(name, options);
+
+		if (args.length == 0) {
+			System.out.println("gtfs4j Command Line Interface");
+			System.out.println();
+			options.usage(name);
+			System.exit(1);
+		}
+
+		ExecutionData data = parser.parse(args);
+		if (data != null) {
+			ExeRunner.run(data);
+		}
+	}
+
+}
